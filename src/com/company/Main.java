5a7d8e77640c744206f8685b018abd271e5070d9
sorensuper10 <soer8814@edu.easj.dk>
2021-09-07 14:32:35 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String day = " Friday ";
+    int date = 3;
+    String month = " September ";
+    int year = 2021;
+    System.out.print("European Format:");
+    System.out.print(day);
+    System.out.print(date);
+    System.out.print(month);
+    System.out.print(year);
+
+    day = " Friday, ";
+    month = "September ";
+    System.out.print(" American Format:");
+    System.out.print(day);
+    System.out.print(month);
+    System.out.print(date);
+    System.out.print(", ");
+    System.out.print(year);
+    }
+}
