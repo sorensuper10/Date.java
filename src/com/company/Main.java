350e68579feacb5376231cf45e15d8e5faf9a74b
sorensuper10 <soer8814@edu.easj.dk>
2021-09-16 11:16:58 +0200

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,20 +10,13 @@ public class Main {
     int date = 3;
     String month = " September ";
     int year = 2021;
-        //TODO samle sout, så de står i én med + operatoren i mellem dem, samt lave en System.out.println(). Alt står på en linje.
-    System.out.print("European Format:"); 
-    System.out.print(day);
-    System.out.print(date);
-    System.out.print(month);
-    System.out.print(year);
+
+    System.out.print("European Format:");
+    System.out.println(day+date+month+year);
 
     day = " Friday, ";
     month = "September ";
-    System.out.print(" American Format:"); //TODO gøre det samme som i European Format med sout. 
-    System.out.print(day);
-    System.out.print(month);
-    System.out.print(date);
-    System.out.print(", ");
-    System.out.print(year);
+    System.out.print("American Format:");
+    System.out.println(day+month+date+", "+year);
     }
 }
